3bf0b64ddc7b12457332edef0dfaac74d99d8af3
Lavanya Pushpakar <lavanya.pushpakar@outlook.com>
2021-05-21 22:11:08 +0530

    Update GuessingGame.c

--- GuessingGame.c
+++ GuessingGame.c
@@ -3,11 +3,11 @@
 #include<time.h>
 int main(){
     int number,guess,nguess;
-    srand(time(0));
-    number = rand()%100+1;
+    srand(time(0));//time(0) returns the time in seconds.Every time when the program runs it generates a different random number
+    number = rand()%100+1;//Generates random number b/w 1-100
     // printf("Number %d\n",number);
     nguess=1;
-
+    //Keep running the loop until the number is guessed
     do{
         printf("Guess the number between 1 to 100\n");
         scanf("%d",&guess);
